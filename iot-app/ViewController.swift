fe8a81beeb3e1371ab3d26b3b555c7a34f94c87f
yokada <yokada@yokadanoMacBook-Air.local>
2021-05-18 00:50:43 +0900

    :tada: Initial commit

---
+++ iot-app/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iot-app
+//
+//  Created by yokada on 2021/05/18.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
