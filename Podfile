a870c7de8d787c209bc2e87341f438c31660638c
yokada <yokada@yokadanoMacBook-Air.local>
2021-05-18 01:18:11 +0900

    :seedling: Amplify authを追加した

---
+++ Podfile
@@ -0,0 +1,17 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'iot-app' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for iot-app
+  pod 'AWSMobileClient'
+  pod 'AWSAuthUI'
+  pod 'AWSUserPoolsSignIn'
+  pod 'AWSS3'
+  pod 'AWSDynamoDB'
+  pod 'AWSAppSync'
+  pod 'Amplify'
+
+end
--- iot-app/ViewController.swift
+++ iot-app/ViewController.swift
@@ -6,12 +6,43 @@
 //
 
 import UIKit
+import AWSMobileClient
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        AWSMobileClient.sharedInstance().initialize { (UserState, error) in
+                    
+                    if let userState = UserState {
+                        switch (UserState) {
+                        case .signedIn:
+                            DispatchQueue.main.async {
+                                print("Sign In")
+                            }
+                            
+                        case .signedOut:
+                            AWSMobileClient.sharedInstance().showSignIn(navigationController: self.navigationController!, { (UserState, error) in
+                                if (error == nil){ //サインイン成功時
+                                    DispatchQueue.main.async {
+                                        print("Sign In")
+                                    }
+
+                                }
+
+                            })
+                        default:
+                            AWSMobileClient.sharedInstance().signOut()
+                        }
+                        
+         
+                    } else if let error = error {
+                        print(error.localizedDescription)
+                    }
+
+                }
     }
 
 
