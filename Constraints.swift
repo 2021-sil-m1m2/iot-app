f71b3777a1f8e60d750ea39aeab788e9596afe47
yokada <yokada@yokadanoMacBook-Air.local>
2021-05-25 06:28:54 +0900

    :dizzy: サインイン画面にデザインを反映した #49

---
+++ Constraints.swift
@@ -0,0 +1,21 @@
+//
+//  Constraints.swift
+//  iot-app
+//
+//  Created by yokada on 2021/05/25.
+//
+
+import Foundation
+import UIKit
+
+extension UIColor{
+    struct Pallet {
+        static let green = #colorLiteral(red: 0.5176470588, green: 0.7333333333, blue: 0.6549019608, alpha: 1)
+        static let green_light = #colorLiteral(red: 0.6862745098, green: 0.8705882353, blue: 0.6117647059, alpha: 1)
+        static let yellow = #colorLiteral(red: 0.9254901961, green: 0.8470588235, blue: 0.5725490196, alpha: 1)
+        static let pink = #colorLiteral(red: 0.9294117647, green: 0.6, blue: 0.6, alpha: 1)
+        static let gray_dark = #colorLiteral(red: 0.2549019608, green: 0.2901960784, blue: 0.2784313725, alpha: 1)
+        static let gray_light = #colorLiteral(red: 0.8431372549, green: 0.8431372549, blue: 0.8431372549, alpha: 1)
+        static let white_bg = #colorLiteral(red: 0.9725490196, green: 0.9725490196, blue: 0.9725490196, alpha: 1)
+    }
+}
