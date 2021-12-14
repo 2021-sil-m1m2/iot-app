c9d1d46bd3df571508178763773a6f409c13d135
yokada <yokada@yokadanoMacBook-Air.local>
2021-12-14 17:53:07 +0900

    :dizzy: planter登録時にplanterNameを入力できるようにした #139

--- iot-app/ViewControllers/ProfileViewController.swift
+++ iot-app/ViewControllers/ProfileViewController.swift
@@ -16,6 +16,7 @@ class ProfileViewController: UIViewController {
 
     @IBOutlet weak var planterIDLabel: UILabel!
     @IBOutlet weak var emailLabel: UILabel!
+    @IBOutlet weak var changePlanterIDButton: UIButton!
     
     let appDelegate = UIApplication.shared.delegate as! AppDelegate
     var appSyncClient: AWSAppSyncClient?
@@ -38,6 +39,9 @@ class ProfileViewController: UIViewController {
             repeats: true )
         
         appSyncClient = appDelegate.appSyncClient
+        
+        // changePlanterIDButtonを非表示にする（実装予定）
+        changePlanterIDButton.isHidden = true
     }
 
     @objc func setUser(_ sender: Timer) {
@@ -60,81 +64,115 @@ class ProfileViewController: UIViewController {
     
     // planterId（name）を新規登録する
     @IBAction func registerPlanterID(_ sender: Any) {
-        print("registerPlanterIdを実行します")
-        print(appDelegate.userid)
-        print(appDelegate.planterName)
-        print(appDelegate.email)
-        // CreateToDoInput関数：入力パラメータを作成
-        let mutationInput = CreatePlanterInput(name: appDelegate.planterName, userId: appDelegate.userid)
         
-        // CreateTodoMutation関数：
-        // AppSyncのcreateTodoに設定されているresolverを実行し，DynamoDBにデータを追加する
-        appSyncClient?.perform(mutation: CreatePlanterMutation(input: mutationInput), resultHandler:  { (result, error) in
-            if let error = error as? AWSAppSyncClientError {
-                print("Error occurred: \(error.localizedDescription )")
-            }
-            if let resultError = result?.errors {
-                print("Error saving the item on server: \(resultError)")
-                return
-            }
-            print("データを追加（runMutation）")
+        var alertTextField: UITextField?
+        let alert = UIAlertController(
+            title: "PlanterNameを登録する",
+            message: "ラズパイのシールを確認してください",
+            preferredStyle: UIAlertController.Style.alert)
+        alert.addTextField(
+            configurationHandler: {(textField: UITextField!) in
+                alertTextField = textField
         })
+        alert.addAction(
+            UIAlertAction(
+                title: "キャンセル",
+                style: UIAlertAction.Style.cancel,
+                handler: nil))
+        // alertの登録ボタンが押された時の処理
+        alert.addAction(
+            UIAlertAction(
+                title: "登録",
+                style: UIAlertAction.Style.default) { _ in
+                if let text = alertTextField?.text {
+//                    self.label1.text = text
+                    self.appDelegate.planterName = text
+                    print("入力された文字列を表示します")
+                    print(self.appDelegate.planterName)
+                    
+                    print("registerPlanterIdを実行します")
+                    print(self.appDelegate.userid)
+                    print(self.appDelegate.planterName)
+                    print(self.appDelegate.email)
+                    // CreateToDoInput関数：入力パラメータを作成
+                    let mutationInput = CreatePlanterInput(name: self.appDelegate.planterName, userId: self.appDelegate.userid)
+
+                    // CreateTodoMutation関数：
+                    // AppSyncのcreateTodoに設定されているresolverを実行し，DynamoDBにデータを追加する
+                    self.appSyncClient?.perform(mutation: CreatePlanterMutation(input: mutationInput), resultHandler:  { (result, error) in
+                        if let error = error as? AWSAppSyncClientError {
+                            print("Error occurred: \(error.localizedDescription )")
+                        }
+                        if let resultError = result?.errors {
+                            print("Error saving the item on server: \(resultError)")
+                            return
+                        }
+                        print("データを追加（runMutation）")
+                    })
+                }
+                    
+            }
+        )
+        self.present(alert, animated: true, completion: nil)
     }
     
+    // changePlanterIDは後回し
     // ログインしているユーザのplanterIdを取得する
-    func getPlanter() {
-        // DynamoDB内のデータを検索する
-        let planter = Planter.keys
-//        let predicate = planter.userID == "b6a130fe-ed4f-49fd-9c71-e6537351a4ad"
-        let predicate = planter.id == "84e057c9-4969-4fe9-9697-c3cdbc95039f"
-            Amplify.API.query(request: .paginatedList(Planter.self, where: predicate, limit: 1000)) { event in
-                switch event {
-                case .success(let result):
-                    switch result {
-                    case .success(let planters):
-                        print("Successfully retrieved list of records: \(planters)")
-                        self.planters.append(contentsOf: planters)
-                    case .failure(let error):
-                        print("Got failed result with \(error.errorDescription)")
-                    }
-                case .failure(let error):
-                    print("Got failed event with error \(error)")
-                }
-            }
-        
-    }
-
-    // planterId（name）を変更する
-    @IBAction func changePlanterID(_ sender: Any) {
-        
-        getPlanter()
-
-        // 次ここから！！0.5秒後ではなく、処理完了後にしたい
-        // planterIdの取得（getPlanterId）に時間がかかるため、0.5秒後に処理する
-        DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
-            // 0.5秒後に実行したい処理
-            print("plantersの中身を表示する")
-            print(self.planters[0].id)
-            self.appDelegate.planterid = self.planters[0].id
-            
-            // planterNameを変更する
-            var planter = Planter(id: self.appDelegate.planterid, name: "new_name", userID: self.appDelegate.userid)
-            Amplify.API.mutate(request: .update(planter)) { event in
-                switch event {
-                case .success(let result):
-                    switch result {
-                    case .success(let planter):
-                        print("Successfully created planter: \(planter)")
-                    case .failure(let error):
-                        print("Got failed result with \(error.errorDescription)")
-                    }
-                case .failure(let error):
-                    print("Got failed event with error \(error)")
-                }
-            }
-        }
-        
-    }
+//    func getPlanter() {
+//        // DynamoDB内のデータを検索する
+//        let planter = Planter.keys
+////        let predicate = planter.userID == "b6a130fe-ed4f-49fd-9c71-e6537351a4ad"
+//        let predicate = planter.id == "84e057c9-4969-4fe9-9697-c3cdbc95039f"
+//            Amplify.API.query(request: .paginatedList(Planter.self, where: predicate, limit: 1000)) { event in
+//                switch event {
+//                case .success(let result):
+//                    switch result {
+//                    case .success(let planters):
+//                        print("Successfully retrieved list of records: \(planters)")
+//                        self.planters.append(contentsOf: planters)
+//                    case .failure(let error):
+//                        print("Got failed result with \(error.errorDescription)")
+//                    }
+//                case .failure(let error):
+//                    print("Got failed event with error \(error)")
+//                }
+//            }
+//
+//    }
+//
+//    // planterId（name）を変更する
+//    @IBAction func changePlanterID(_ sender: Any) {
+//
+//        // getPlanter()
+//
+//        // 0.5秒後ではなく、処理完了後にしたい
+//        // planterIdの取得（getPlanterId）に時間がかかるため、0.5秒後に処理する
+//        DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
+//            // 0.5秒後に実行したい処理
+//            print("plantersの中身を表示する")
+//            print(self.planters[0].id)
+//            self.appDelegate.planterid = self.planters[0].id
+//            print(self.appDelegate.planterid)
+//
+//            // planterNameを変更する
+////            var planter = Planter(id: self.appDelegate.planterid, name: "new_name_2", userID: self.appDelegate.userid)
+//            let planternew = Planter(name: "new_name_3", userID: self.appDelegate.userid)
+//            Amplify.API.mutate(request: .update(planternew)) { event in
+//                switch event {
+//                case .success(let result):
+//                    switch result {
+//                    case .success(let planter):
+//                        print("Successfully created planter: \(planter)")
+//                    case .failure(let error):
+//                        print("Got failed result with \(error.errorDescription)")
+//                    }
+//                case .failure(let error):
+//                    print("Got failed event with error \(error)")
+//                }
+//            }
+//        }
+//
+//    }
     
     @IBAction func signOut(_ sender: Any) {
         signOutLocally()
